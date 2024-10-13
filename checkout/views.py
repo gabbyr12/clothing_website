617e93d671c74525e474a5f2bac66db9604f7e43
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2024-10-13 14:30:32 +0100

    fly

--- checkout/views.py
+++ checkout/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def fly(request):
+    return render(request, 'fly.html')
--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('checkout/', include('checkout.urls')),
+    path('bag/', include('checkout.urls')),
     path('admin/', admin.site.urls),
 ]
